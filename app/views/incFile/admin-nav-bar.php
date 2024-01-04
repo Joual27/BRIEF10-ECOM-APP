7f263673610f0a9068ccd1fa7e56d216786e703d
Imiul <amiul@proton.me>
2024-01-04 09:28:00 +0100

    Add Admin Pages UI

---
+++ app/views/incFile/admin-nav-bar.php
@@ -0,0 +1,30 @@
+<aside class="bg-[#1d3557] h-[100vh] w-[20%]">
+            <ul class="p-5 mt-7">
+
+                <!-- Dashboard Logo -->
+                <img class="h-6" src="" alt="" > 
+
+                <li class="my-2 pt-4" >
+                    <a href="/ECOM/Admin/categories" class="text-lg font-medium w-[full] rounded h-[40px] text-white flex items-center p-5 py-6 group bg-[#778da9] hover:bg-indigo-200 hover:bg-opacity-30 bg-opacity-20">
+                        <img class="h-6 sm:w-6 w-full" src="<?= URLROOT."/img/icons/category.png" ?>" alt="" > 
+                        <span class="hidden sm:inline-block pl-4">Categories</span>
+                    </a>
+                </li>
+
+                <li class="my-2" >
+                    <a href="/ECOM/Admin/products" class="text-lg font-medium w-[full] rounded h-[40px] text-white flex items-center p-5 group bg-[#778da9] hover:bg-indigo-200 hover:bg-opacity-30 bg-opacity-20">
+                        <img class="h-6 sm:w-6 w-full" src="<?= URLROOT."/img/icons/products.png" ?>" alt="" > 
+                        <span class="hidden sm:inline-block pl-4">Products</span>
+                    </a>
+                </li>
+
+                <li class="my-2" >
+                    <a href="?logout=true" class="bg-[#ef233c] h-10 w-[50%] sm:w-full flex items-center rounded p-5 text-[#fff] hover:bg-[#ff8fab] font-bold m-auto mt-10">
+                        <img class="h-6 sm:w-6 w-full" src="<?= URLROOT."/img/icons/logout.png" ?>" alt="" > 
+                        <span class="hidden sm:inline-block pl-4">Log Out</span>
+                    </a>
+                </li>
+            </ul>
+        </aside>
+
+        
